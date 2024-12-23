7898c44b5a2351a7003059f082d96e42590c12ed
kefir228 <oleg05karp@gmail.com>
2024-12-23 20:04:24 +0200

    add functional to admin

--- src/components/AdminPage/Admin.tsx
+++ src/components/AdminPage/Admin.tsx
@@ -4,18 +4,8 @@ import { useNavigate } from 'react-router-dom'
 import { signOut } from 'slicers/registrationSlice'
 import { Link } from 'react-router-dom'
 import { RootState } from 'store'
-
-enum Title {
-    Change = 'Change Card',
-    Add = 'Create New Card',
-    Delete = 'Delete Card',
-}
-
-const titleLinks: Record<Title, string> = {
-    [Title.Add]: '/add',
-    [Title.Change]: '/change',
-    [Title.Delete]: '/delete'
-}
+import { useState } from 'react'
+import { Card } from 'slicers/cardSlice'
 
 export const Admin = () => {
     const dispatch = useDispatch()
@@ -29,13 +19,31 @@ export const Admin = () => {
         navigate('/')
     }
 
-    const allCards = useSelector((state: RootState) => state.card.cards)
+    const allCards: Card[] = useSelector((state: RootState) => state.card.cards)
+    const [menuVisible, setMenuVisible] = useState(false)
+    const [selectedCardId, setSelectedCardId] = useState<string | null>(null)
+    const [menuPosition, setMenuPosition] = useState<{ top: number, left: number }>({ top: 0, left: 0 })
+
+    const handleCardClick = (cardId: string, event: React.MouseEvent) => {
+        const { clientX, clientY } = event;
+        setSelectedCardId(cardId)
+        setMenuPosition({ top: clientY, left: clientX })
+        setMenuVisible(true)
+    }
+
+    const closeModal = () => {
+        setMenuVisible(false)
+        setSelectedCardId(null)
+    }
 
     return (
-
         <div className='admin'>
-
             <ul className='admin__header'>
+                <li>
+                    <button className='admin__logout-button'>
+                        <Link to={'/create'}>Create New Card</Link>
+                    </button>
+                </li>
                 <li>
                     <h1>Admin Dashboard</h1>
                 </li>
@@ -47,17 +55,11 @@ export const Admin = () => {
             </ul>
 
 
-            <ul>
-                {Object.values(Title).map((item) => (
-                    <li key={item}>
-                        <Link to={titleLinks[item]}>{item}</Link>
-                    </li>
-                ))}
-            </ul>
+
             <div className='footer'>
                 <ul className='footer__list'>
                     {allCards.map((card) => (
-                        <li className='footer__card' key={card.id}>
+                        <li className='footer__card' key={card.id} onClick={(event) => handleCardClick(card.id, event)}>
                             <img src={card.image} alt={card.name} className='footer__image' />
                             <h2 className='footer__name'>{card.name}</h2>
                             <p className='footer__price'>
@@ -76,6 +78,14 @@ export const Admin = () => {
                     ))}
                 </ul>
             </div>
+            {menuVisible && selectedCardId && (
+                <div className='admin__menu' style={{ top: `${menuPosition.top}px`, left: `${menuPosition.left}px` }}>
+                    <button >Редагувати картку</button>
+                    <button >Видалити картку</button>
+                    <button >Додати знижку</button>
+                    <ion-icon name="close-outline" onClick={closeModal} style={{ fontSize: '20px', cursor: 'pointer' }}></ion-icon>
+                </div>
+            )}
         </div>
     )
 }
